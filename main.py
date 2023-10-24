1bb61797349ce1e4088fc9d9b41e6b97fe9a7e45
vanhungsi <vanhungsi@gmail.com>
2023-10-24 10:47:58 +0700

    Day 22: Render the app title in 2 columns

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
@@ -13,9 +13,18 @@ with col2:
     content = """Hello, I'm Van Hung Si, a 31-year-old professional with a passion for technology and a knack for problem-solving. By day, I lead a dynamic team as a Warehouse Document Team Leader, ensuring seamless operations and meticulous record-keeping.
 
 In my spare time, I've immersed myself in the world of Python programming, leveraging its power to tackle various challenges and create solutions that streamline processes and enhance efficiency.
-
-Here are a few Python projects I've worked on:
     """
     st.info(content)
 content2 = """Below you can find some of the apps I have built in Python. Feel free to contact me!"""
 st.text(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv",sep=",")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
